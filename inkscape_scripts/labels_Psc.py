8505ff5cf684baee70ea642dd1042ecac8ceb7c2
Giacomo Menegatti <giacomo.menegatti@studenti.unipd>
2025-04-14 12:14:17 +0200

    changed structure

--- inkscape_scripts/labels_Psc.py
+++
@@ -1,6 +0,0 @@
-# Named stars labels
-
-# Constellation parts labels
-
-# Ecliptic label
-text('ecliptic', (0.22*canvas.width, 0.36*canvas.height), font_size='10pt',text_anchor='middle', font_family='Z003', fill='#ff0000')
