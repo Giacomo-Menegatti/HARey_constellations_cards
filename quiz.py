ac79f2b2e97d34f6273e46452a4702536544ddd3
Giacomo Menegatti <giacomo.menegatti@studenti.unipd>
2025-04-19 19:10:19 +0200

    Quiz with full names

--- quiz.py
+++ quiz.py
@@ -2,9 +2,12 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import os
 import random
+from HARey_constellation_cards.loader import load_names
 
 # Folder of the cards
+path = '/home/menegattig/Desktop/HARey_constellations_cards'
 folder = 'Quiz_cards'
+folder = path + '/' + folder
 
 # Number of questions to ask
 number_of_questions = 3
@@ -24,8 +27,14 @@ for file in os.listdir(folder):
         con_lines[id] = file
 
 ids = list(con_bare.keys())
+names = load_names(f'{path}/names.csv')
 
 current_question_index = 0
+
+# Create lists to store guessed right and wrong answers
+guessed_right = []
+guessed_wrong = []
+
 buttons = []
 
 def load_image(path):
@@ -48,20 +57,26 @@ def load_question():
 
     image_label.config(image=con_bare_image)
     for i, option in enumerate(options):
-        buttons[i].config(text=option, state="normal")
+        buttons[i].config(text=names[option], state="normal", **button_style)
 
 def check_answer(selected_index):
-    global correct_index
+    global correct_index, options, guessed_right, guessed_wrong
     selected_button = buttons[selected_index]
 
     # Disable all buttons
     for btn in buttons:
-        btn.config(state="disabled")
+        btn.config(state="disabled") 
 
     # Set colors
     if selected_index == correct_index:
+        guessed_right.append(options[selected_index])
+
+        # Highlight the selected button
         selected_button.config(bg="green")
     else:
+        guessed_wrong.append(options[correct_index])
+
+        # Highlight the selected button and the correct one
         selected_button.config(bg="red")
         buttons[correct_index].config(bg="green")
 
@@ -84,8 +99,32 @@ def next_question():
         load_question()
     else:
         # If quiz is finished, disable buttons and show the final message
-        image_label.config(image="", text="Quiz Finished!", font=("Arial", 24), bg="black", fg="white")
+        final_message = f"Quiz Finished!\n\n\n"
+
+        if len(guessed_right)> 0:
+            final_message += f"You guessed correctly:\n"
+            for id in guessed_right:
+                final_message += f"{names[id].replace('\n', ' ')}\n"
+
+        if len(guessed_wrong) > 0:
+            final_message += f"\nYou did not recognize:\n"
+            for id in guessed_wrong:
+                final_message += f"{names[id].replace('\n', ' ')}\n"
+
+        image_label.config(anchor='center', image="", text=final_message, font=("Arial", 24), bg="black", fg="white")
         button_frame.pack_forget()
+
+button_style = {
+    'bg': 'black',
+    'fg': 'white',
+    'activebackground': '#333333',
+    'activeforeground': 'white',
+    'relief': 'raised',
+    'bd': 2,
+    'font': ('Helvetica', 12),
+    'height': 2,
+    'highlightthickness': 0,
+}
 
 # Setup GUI
 root = tk.Tk()
@@ -95,7 +134,7 @@ root.configure(bg="black")
 
 # Create a frame to contain the image
 con_bare_image = None
-image_label = tk.Label(root)
+image_label = tk.Label(root, bg='black')
 image_label.pack(pady=10)
 
 # Create a frame for the buttons
@@ -104,7 +143,7 @@ button_frame.pack(pady=10)
 
 # Create answer buttons
 for i in range(number_of_options):
-    btn = tk.Button(button_frame, text="", font=("Arial", 14), command=lambda i=i: check_answer(i))
+    btn = tk.Button(button_frame, text="", command=lambda i=i: check_answer(i), **button_style)
     btn.grid(row=0, column=i, padx=10, pady=10)
     buttons.append(btn)
 
